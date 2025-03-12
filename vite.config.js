7759314a492a6c37ff2f1655560fa48e335ecbcb
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-11 23:33:15 -0600

    Add base repo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/React_Form_Validations/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
