4f262aac4cf9ee5b6a6ddd9c9bb5b94690d367f1
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-11 23:35:27 -0600

    change base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/React_Form_Validations/",
+  base: "/alexter2003.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
